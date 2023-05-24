83fff28999e8e00a452b452d04ddc4ca95d3f011
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-05-24 17:09:35 +0200

    added main pages and components

---
+++ src/components/SinglePokemon.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SinglePokemon() {
+  return <div>SinglePokemon</div>;
+}
---
+++ src/pages/Highscore.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Highscore() {
+  return <div>Highscore</div>;
+}
