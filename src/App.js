03f10b8d05b2f0de9cdfdc78359007f1f52f6b76
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-05-25 16:25:46 +0200

    added axios fetch from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import { Route, Routes } from "react-router";
+// import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]); // Rename state variable
+  // const { pathname } = useLocation();
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      // Define asynchronous function
+      try {
+        const response = await axios.get("http://localhost:8080/pokemon"); // Make GET request to localhost:8080/pokemon
+        setPokemon(response.data); // Set state with response data
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchPokemon(); // Call asynchronous function
+  }, []);
+
+  // useEffect(() => {
+  //   window.scrollTo(0, 0);
+  // }, [pathname]);
+
+  console.log(pokemon);
   return (
     <div className="App">
       <header className="App-header">
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,3 +3,4 @@ import React from "react";
 export default function Search() {
   return <div>Search</div>;
 }
+ 
