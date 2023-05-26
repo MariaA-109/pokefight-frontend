7b1ac8b3d70ba809d31244664b89c462bb279aab
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-05-26 18:01:41 +0200

    working on pagination

--- src/pages/AllPokemon.js
+++ src/pages/AllPokemon.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import SinglePokemon from "../components/SinglePokemon";
+import Pagination from "../components/Pagination";
 
 export default function AllPokemon({ pokemondb }) {
-  console.log("Page: AllPokemon:", pokemondb);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pokemonPerPage] = useState(10);
+  console.log("Page: AllPokemon1:", pokemondb);
+  const indexOfLastPokemon = currentPage * pokemonPerPage;
+  const indexOfFirstPokemon = indexOfLastPokemon - pokemonPerPage;
+  const currentPokemon = pokemondb.slice(
+    indexOfFirstPokemon,
+    indexOfLastPokemon
+  );
+  console.log("Page: AllPokemon2:", currentPokemon);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  console.log("Page: AllPokemon3:", currentPokemon);
   return (
     <>
       <div>AllPokemon</div>
-      {pokemondb.length > 0 ? (
-        pokemondb.map((pokemon) => (
+      {currentPokemon.length > 0 ? (
+        currentPokemon.map((pokemon) => (
           <SinglePokemon key={pokemon.id} pokemon={pokemon} />
         ))
       ) : (
         <h1>No Pokemon Found</h1>
       )}
+
+      <Pagination
+        postsPerPage={pokemonPerPage}
+        totalPosts={pokemondb.length}
+        paginate={paginate}
+      />
     </>
   );
 }
