a33d9034f98144e5cf77b05fce2d1abf5b2bae0e
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-05-26 16:23:29 +0200

    updated AllPokemon

--- src/pages/AllPokemon.js
+++ src/pages/AllPokemon.js
@@ -1,15 +1,21 @@
 import SinglePokemon from "../components/SinglePokemon";
+import Filter from "../components/Filter";
+import Search from "../components/Search";
 
 export default function AllPokemon({ pokemondb }) {
   return (
-    <div className="grid grid-cols-3 justify-center mx-1 my-1">
-      {pokemondb.length > 0 ? (
-        pokemondb.map((item) => {
-          return <SinglePokemon item={item} key={item.id} />;
-        })
-      ) : (
-        <h1>No Pokemon Found</h1>
-      )}
-    </div>
+    <>
+      <Search pokemondb={pokemondb} />
+      <Filter pokemondb={pokemondb} />
+      <div className="grid grid-cols-3 justify-center mx-1 my-1">
+        {pokemondb.length > 0 ? (
+          pokemondb.map((item) => {
+            return <SinglePokemon item={item} key={item.id} />;
+          })
+        ) : (
+          <h1>No Pokemon Found</h1>
+        )}
+      </div>
+    </>
   );
 }
