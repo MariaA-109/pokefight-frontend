b0a988f0a6b8fb38741c67346fded1e3b2d847d7
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-05-30 14:22:39 +0200

    added final pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,26 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  const pageNumbers = [];
+const Pagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+  onNextPage,
+  onPrevPage,
+}) => {
+  const visiblePageNumbers = [];
+  const maxVisiblePages = 5;
+  const halfMaxVisiblePages = Math.floor(maxVisiblePages / 2);
 
-  for (let i = 1; i <= totalPages; i++) {
-    pageNumbers.push(i);
+  // Logic to determine the visible page numbers
+  let startPage = Math.max(1, currentPage - halfMaxVisiblePages);
+  let endPage = Math.min(totalPages, startPage + maxVisiblePages - 1);
+  startPage = Math.max(1, endPage - maxVisiblePages + 1);
+
+  for (let i = startPage; i <= endPage; i++) {
+    visiblePageNumbers.push(i);
   }
 
   return (
-    <nav className="flex justify-center my-4">
-      <ul className="pagination">
-        {pageNumbers.map((number) => (
+    <nav>
+      <ul className="pagination flex justify-center  my-4">
+        <li className="px-10 py-1">
+          <button
+            onClick={onPrevPage}
+            disabled={currentPage === 1}
+            className="disabled:opacity-50"
+          >
+            Prev
+          </button>
+        </li>
+        {visiblePageNumbers.map((number) => (
           <li
             key={number}
-            className={`px-2 py-1 ${
-              currentPage === number ? "bg-blue-500 text-white" : ""
+            className={`px-4 py-1 ${
+              currentPage === number ? "bg-rose-500 text-white" : ""
             }`}
           >
             <button onClick={() => onPageChange(number)}>{number}</button>
           </li>
         ))}
+        <li className="px-10 py-1">
+          <button
+            onClick={onNextPage}
+            disabled={currentPage === totalPages}
+            className="disabled:opacity-50"
+          >
+            Next
+          </button>
+        </li>
       </ul>
     </nav>
   );
@@ -30,6 +61,8 @@ Pagination.propTypes = {
   currentPage: PropTypes.number.isRequired,
   totalPages: PropTypes.number.isRequired,
   onPageChange: PropTypes.func.isRequired,
+  onNextPage: PropTypes.func.isRequired,
+  onPrevPage: PropTypes.func.isRequired,
 };
 
 export default Pagination;
--- src/pages/AllPokemon.js
+++ src/pages/AllPokemon.js
@@ -6,23 +6,27 @@ import Pagination from "../components/Pagination";
 
 export default function AllPokemon({ pokemondb }) {
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage] = useState(9);
-
-  // Logic to get the current items to display
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = pokemondb.slice(indexOfFirstItem, indexOfLastItem);
-
+  const itemsPerPage = 9;
   const totalPages = Math.ceil(pokemondb.length / itemsPerPage);
 
   const onPageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
-  return (
-    <>
-      <Search pokemondb={pokemondb} />
-      <Filter pokemondb={pokemondb} />
+  const onNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const onPrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const renderPokemon = () => {
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = pokemondb.slice(indexOfFirstItem, indexOfLastItem);
+
+    return (
       <div className="grid grid-cols-3 justify-center mx-1 my-1">
         {currentItems.length > 0 ? (
           currentItems.map((item) => (
@@ -32,10 +36,22 @@ export default function AllPokemon({ pokemondb }) {
           <h1>No Pokemon Found</h1>
         )}
       </div>
+    );
+  };
+
+  return (
+    <>
+      <Search pokemondb={pokemondb} />
+      <Filter pokemondb={pokemondb} />
+
+      {renderPokemon()}
+
       <Pagination
         currentPage={currentPage}
         totalPages={totalPages}
         onPageChange={onPageChange}
+        onNextPage={onNextPage}
+        onPrevPage={onPrevPage}
       />
     </>
   );
