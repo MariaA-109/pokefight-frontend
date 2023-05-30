fd0e6299ff968e6160349d22832dc417a162ab6d
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-05-30 11:09:15 +0200

    working on pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,122 +1,35 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+  for (let i = 1; i <= totalPages; i++) {
     pageNumbers.push(i);
   }
 
   return (
-    <>
-      <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm">
-        <ul className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-          {pageNumbers.map((number) => (
-            <li
-              key={number}
-              className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-            >
-              <a
-                onClick={() => paginate(number)}
-                href="!#"
-                className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-              >
-                {number}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-
-      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-        <div className="flex flex-1 justify-between sm:hidden">
-          <a
-            href="#"
-            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+    <nav className="flex justify-center my-4">
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li
+            key={number}
+            className={`px-2 py-1 ${
+              currentPage === number ? "bg-blue-500 text-white" : ""
+            }`}
           >
-            Previous
-          </a>
-          <a
-            href="#"
-            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
-            Next
-          </a>
-        </div>
-        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-          <div>
-            <p className="text-sm text-gray-700">
-              Showing <span className="font-medium">1</span> to{" "}
-              <span className="font-medium">10</span> of{" "}
-              <span className="font-medium">97</span> results
-            </p>
-          </div>
-          <div>
-            <nav
-              className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-              aria-label="Pagination"
-            >
-              <a
-                href="#"
-                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                <span className="sr-only">Previous</span>
-                {/* <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" /> */}
-              </a>
-              {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-              <a
-                href="#"
-                aria-current="page"
-                className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                1
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                2
-              </a>
-              <a
-                href="#"
-                className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-              >
-                3
-              </a>
-              <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-                ...
-              </span>
-              <a
-                href="#"
-                className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-              >
-                8
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                9
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                10
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                <span className="sr-only">Next</span>
-                {/* <ChevronRightIcon className="h-5 w-5" aria-hidden="true" /> */}
-              </a>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </>
+            <button onClick={() => onPageChange(number)}>{number}</button>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
+};
+
+Pagination.propTypes = {
+  currentPage: PropTypes.number.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
 };
 
 export default Pagination;
--- src/pages/AllPokemon.js
+++ src/pages/AllPokemon.js
@@ -1,37 +1,41 @@
 import React, { useState } from "react";
 import SinglePokemon from "../components/SinglePokemon";
+import Filter from "../components/Filter";
+import Search from "../components/Search";
 import Pagination from "../components/Pagination";
 
 export default function AllPokemon({ pokemondb }) {
   const [currentPage, setCurrentPage] = useState(1);
-  const [pokemonPerPage] = useState(10);
-  console.log("Page: AllPokemon1:", pokemondb);
-  const indexOfLastPokemon = currentPage * pokemonPerPage;
-  const indexOfFirstPokemon = indexOfLastPokemon - pokemonPerPage;
-  const currentPokemon = pokemondb.slice(
-    indexOfFirstPokemon,
-    indexOfLastPokemon
-  );
-  console.log("Page: AllPokemon2:", currentPokemon);
+  const [itemsPerPage] = useState(9);
 
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  // Logic to get the current items to display
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = pokemondb.slice(indexOfFirstItem, indexOfLastItem);
 
-  console.log("Page: AllPokemon3:", currentPokemon);
+  const totalPages = Math.ceil(pokemondb.length / itemsPerPage);
+
+  const onPageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <>
-      <div>AllPokemon</div>
-      {currentPokemon.length > 0 ? (
-        currentPokemon.map((pokemon) => (
-          <SinglePokemon key={pokemon.id} pokemon={pokemon} />
-        ))
-      ) : (
-        <h1>No Pokemon Found</h1>
-      )}
-
+      <Search pokemondb={pokemondb} />
+      <Filter pokemondb={pokemondb} />
+      <div className="grid grid-cols-3 justify-center mx-1 my-1">
+        {currentItems.length > 0 ? (
+          currentItems.map((item) => (
+            <SinglePokemon item={item} key={item.id} />
+          ))
+        ) : (
+          <h1>No Pokemon Found</h1>
+        )}
+      </div>
       <Pagination
-        postsPerPage={pokemonPerPage}
-        totalPosts={pokemondb.length}
-        paginate={paginate}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={onPageChange}
       />
     </>
   );
