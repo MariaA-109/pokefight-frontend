f5da21831bc8f22e5b1bf68ed2396592d451702a
Christoph Nientied <christophnientied@googlemail.com>
2023-05-30 15:39:31 +0200

    inserted searchbar into allpokemon.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,105 @@
-import React from "react";
+// import { useState } from "react";
 
-export default function Search() {
-  return <div>Search</div>;
-}
- 
+// export default function Search({ pokemondb }) {
+//   const [inputValue, setInputValue] = useState("");
+//   const [filteredPokemon, setFilteredPokemon] = useState([]);
+
+//   const handleSearch = (event) => {
+//     event.preventDefault();
+
+//     const filteredResults = pokemondb.filter((item) =>
+//       item.name.english.toLowerCase().includes(inputValue.toLowerCase())
+//     );
+//     setFilteredPokemon(filteredResults);
+//   };
+
+//   return (
+//     <form>
+//     <input
+//       className="searchBar"
+//       value={inputValue}
+//       onChange={(e) => setInputValue(e.target.value)}
+//       type="text"
+//       placeholder="Search..."
+//     />
+//     <button onClick={handleSearch}>Search</button>
+//   </form>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from "react";
+
+// export default function Search({ setQuery, pokemondb }) {
+//   const [inputValue, setInputValue] = useState("");
+
+//   const addQuery = (event) => {
+//     event.preventDefault();
+//     if (inputValue.trim() === "") {
+//       alert("Your PokeSearch empty!");
+//       // return;
+//     }
+//     setQuery(inputValue.trim());
+//     setInputValue("");
+//   };
+
+//   return (
+//     <div>
+//       <form>
+//         <input
+//           className="searchBar"
+//           value={inputValue}
+//           onChange={(e) => setInputValue(e.target.value)}
+//           type="text"
+//           placeholder="Search..."
+//         />
+//         <button onSubmit={addQuery}>Search</button>
+//       </form>
+
+//       <ul>
+//         {pokemondb
+//         .filter((singlePokemon) =>{
+//           return inputValue.toLowerCase() === '' 
+//           ? singlePokemon.name.english 
+//           : singlePokemon.name.english.toLowerCase().includes(inputValue)
+//           })
+//           .map((singlePokemon) => (
+//           <li>{singlePokemon.name.english}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
--- src/pages/AllPokemon.js
+++ src/pages/AllPokemon.js
@@ -1,38 +1,42 @@
-import SinglePokemon from "../components/SinglePokemon";
-import Filter from "../components/Filter";
+import SinglePokemon from "../components/SinglePokemon"; 
+import { useState } from "react";
 import Search from "../components/Search";
 
 export default function AllPokemon({ pokemondb }) {
+  const [inputValue, setInputValue] = useState("");
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    const filteredResults = pokemondb.filter((item) =>
+      item.name.english.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    setFilteredPokemon(filteredResults);
+  };
+
   return (
-    <>
-      <Search pokemondb={pokemondb} />
-      <Filter pokemondb={pokemondb} />
+    <div>
+      <form>
+        <input
+          className="searchBar"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          type="text"
+          placeholder="Search..."
+        />
+        <button onClick={handleSearch}>Search</button>
+      </form>
+
       <div className="grid grid-cols-3 justify-center mx-1 my-1">
-        {pokemondb.length > 0 ? (
-          pokemondb.map((item) => {
-            return <SinglePokemon item={item} key={item.id} />;
-          })
+        {filteredPokemon.length > 0 ? (
+          filteredPokemon.map((item) => (
+            <SinglePokemon item={item} key={item.id} />
+          ))
         ) : (
           <h1>No Pokemon Found</h1>
         )}
       </div>
-
-{/* import React from "react";
-import SinglePokemon from "../components/SinglePokemon";
-
-export default function AllPokemon({ pokemondb }) {
-  console.log("Page: AllPokemon:", pokemondb);
-  return (
-    <>
-      <div>AllPokemon</div>
-      {pokemondb.length > 0 ? (
-        pokemondb.map((pokemon) => (
-          <SinglePokemon key={pokemon.id} pokemon={pokemon} />
-        ))
-      ) : (
-        <h1>No Pokemon Found</h1>
-      )} */}
-
-    </>
+    </div>
   );
 }
