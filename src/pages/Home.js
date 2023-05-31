66f5f8aa3d27b3e3114e378bd20b4f78e2e4083e
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-05-31 17:00:13 +0200

    changes in pokemondetail

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
-// import { useState, useEffect } from "react";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// // import { useState, useEffect } from "react";
 
-import "../css/home.css";
-// import img1 from "../img/pokemon.png";
-// import img2 from "../img/glurak.png";
-// import sound from "../img/Opening.mp3";
+// import "../css/home.css";
+// // import img1 from "../img/pokemon.png";
+// // import img2 from "../img/glurak.png";
+// // import sound from "../img/Opening.mp3";
 
-function play() {
-  new Audio(sound).play();
-}
+// function play() {
+//   new Audio(sound).play();
+// }
 
-export default function Home() {
-  return (
-    <>
-      <div className="wrapper">
-        <div className="container">
-          <div className="pokemon"></div>
+// export default function Home() {
+//   return (
+//     <>
+//       <div className="wrapper">
+//         <div className="container">
+//           <div className="pokemon"></div>
 
-          <div className="red-section">
-            <img className="pokemon" src={img1} alt="Pokemon png" />
-            <div>
-              <div>
-                <img className="glurak" src={img2} alt="Glurak png" />
-              </div>
-            </div>
+//           <div className="red-section">
+//             <img className="pokemon" src={img1} alt="Pokemon png" />
+//             <div>
+//               <div>
+//                 <img className="glurak" src={img2} alt="Glurak png" />
+//               </div>
+//             </div>
 
-            <div className="button-container">
-              <Link onClick={play} to="/allpokemon" className="button"></Link>
-            </div>
-          </div>
-          <div className="white-section"></div>
-        </div>
-      </div>
-    </>
-  );
-}
+//             <div className="button-container">
+//               <Link onClick={play} to="/allpokemon" className="button"></Link>
+//             </div>
+//           </div>
+//           <div className="white-section"></div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
--- src/pages/PokemonDetail.js
+++ src/pages/PokemonDetail.js
@@ -1,5 +1,17 @@
+import { useParams } from "react-router-dom";
 import React from "react";
 
-export default function PokemonDetail() {
-  return <div>PokemonDetail</div>;
+function PokemonDetail({ pokemondb }) {
+  console.log("pokemondb", pokemondb);
+  const { pokeID } = useParams();
+  const singlePokemon = pokemondb.find((item) => item.id == pokeID);
+
+  return (
+    <div>
+      <h3>{singlePokemon.name}</h3>
+      <p>{singlePokemon.type}</p>
+    </div>
+  );
 }
+
+export default PokemonDetail;
