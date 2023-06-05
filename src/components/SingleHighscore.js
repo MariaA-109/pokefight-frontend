77068e843836ac88ae6fb42490fd79024bceb787
Christoph Nientied <christophnientied@googlemail.com>
2023-06-05 18:21:02 +0200

    finalizing highcore page

--- src/components/SingleHighscore.js
+++ src/components/SingleHighscore.js
@@ -3,7 +3,7 @@ export default function SingleHighscore( {score, item, rank} ) {
 
   return (
     <>
-    <div className="grid grid-cols-3 justify-center mx-1 my-1 m-3 border p-3 rounded-md w-104  ">
+    <div className="grid grid-cols-3 justify-center mx-1 my-1 m-3 border p-3 rounded-md w-104  text-lg">
         <li>{rank}</li>
         <li>{item.winner}</li>
         <li>{`${item.total_wins}`}</li>
--- src/pages/Highscore.js
+++ src/pages/Highscore.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SingleHighscore from "../components/SingleHighscore";
+import { Link } from "react-router-dom";
 
 export default function Highscore({ score }) {
   const sortedScore = [...score]; // Create a new array to avoid mutating the original data
@@ -12,11 +13,19 @@ export default function Highscore({ score }) {
       <img
         src={require("../img/goldenTrophy.jpeg")}
         alt="Winner Trophy"
-        style={{ width: "160px", height: "auto", margin: "0 auto", borderRadius: 5 }}
+        style={{
+          width: "160px",
+          height: "auto",
+          margin: "0 auto",
+          borderRadius: 5,
+        }}
       />
       <br></br>
 
-      <i>"No matter how hard things get, the real value of winning <br></br>is the experience you gain in battling." - Ash Ketchum</i>
+      <i className="text-lg">
+        "No matter how hard things get, the real value of winning <br></br>is
+        the experience you gain in battling." - Ash Ketchum
+      </i>
       <br></br>
       <br></br>
       <div className="grid grid-cols-3 justify-center mx-1 my-1 m-3 border p-3 rounded-md w-104">
@@ -28,13 +37,31 @@ export default function Highscore({ score }) {
       {sortedScore.length > 0 ? (
         <ol>
           {sortedScore.map((item, index) => (
-            <SingleHighscore key={index} item={item} score={score} rank={index + 1}/>
+            <SingleHighscore
+              key={index}
+              item={item}
+              score={score}
+              rank={index + 1}
+            />
           ))}
         </ol>
       ) : (
         <h1>No Highscores available yet</h1>
       )}
+      <br></br>
+      <div className="grid grid-cols-2">
+        <Link to="../arena/" className="flex ml-10 mx-1 my-1">
+          <button className="bg-rose-500 rounded-sm text-white text-lg h-12 w-40 font-bold justify-center">
+            Back to Arena
+          </button>
+        </Link>
+        <Link to="../allpokemon/" className="flex ml-10 mx-1 my-1">
+          <button className="bg-rose-500 rounded-sm text-white text-lg h-12 w-40 font-bold justify-center">
+            Back to Pokedex
+          </button>
+        </Link>
+      </div>
+      <br></br>
     </>
   );
 }
-
