6f45b7cbe02371aa002e5d4f3124e9bbb1f144cf
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-08-03 09:42:38 +0200

    added fight logic

--- src/pages/Arena.js
+++ src/pages/Arena.js
@@ -11,8 +11,9 @@ export default function Arena() {
   const location = useLocation();
   const { chosenPokemon } = location.state;
   // console.log("chosenPokemon: " + chosenPokemon);
-  const [randomPokemon, setRandomPokemon] = useState(null);
+  const [randomPokemon, setRandomPokemon] = useState("null");
   const [isLoading, setIsLoading] = useState(true);
+  const [winner, setWinner] = useState(null);
 
   // API-Daten abrufen
   useEffect(() => {
@@ -23,6 +24,7 @@ export default function Arena() {
           `https://pokeapi.co/api/v2/pokemon/${randomId}`
         );
         setRandomPokemon(response.data);
+        console.log("randomPokemon:", randomPokemon.stats[1].base_stat);
         setIsLoading(false);
       } catch (error) {
         console.log("Fehler bei der Datenabfrage", error);
@@ -33,6 +35,68 @@ export default function Arena() {
     fetchAPI();
   }, []);
 
+  //Battle logic ////////
+  const [hpPokemonState, setHPPokemonState] = useState(
+    chosenPokemon.stats[0].base_stat
+  );
+  console.log("hp: ", hpPokemonState);
+
+  const [attackPokemonState, setAttackPokemonState] = useState(
+    chosenPokemon.stats[1].base_stat
+  );
+  console.log("atttack: ", attackPokemonState);
+
+  const [defensePokemonState, setDefensePokemonState] = useState(
+    chosenPokemon.stats[2].base_stat
+  );
+  console.log("defense: ", defensePokemonState);
+
+  const [speedPokemonState, setSpeedPokemonState] = useState(
+    chosenPokemon.stats[5].base_stat
+  );
+  console.log("speed: ", speedPokemonState);
+
+  const [secondPokemonState, setSecondPokemonState] = useState(null);
+  console.log("secondPokemon: ", secondPokemonState);
+
+  useEffect(() => {
+    if (randomPokemon.stats && randomPokemon.stats[0]) {
+      const secondPokemonBaseStat = randomPokemon.stats[0].base_stat;
+      setSecondPokemonState(secondPokemonBaseStat);
+      console.log("secondPokemon: ", secondPokemonBaseStat);
+    } else if (randomPokemon.stats && randomPokemon.stats[1]) {
+      const attackPokemonBaseStat = randomPokemon.stats[1].base_stat;
+      //setSecondPokemonState(attackPokemonBaseStat);
+      console.log("attacksecondPokemon: ", attackPokemonBaseStat);
+    } else if (randomPokemon.stats && randomPokemon.stats[2]) {
+      const defensePokemonBaseStat = randomPokemon.stats[2].base_stat;
+      //setSecondPokemonState(defensePokemonBaseStat);
+      console.log("defensesecondPokemon: ", defensePokemonBaseStat);
+    }
+  }, [randomPokemon]);
+
+  function handleAttack() {
+    const newFirstPokemonState = hpPokemonState - 30;
+    setHPPokemonState(newFirstPokemonState);
+
+    if (newFirstPokemonState <= 0) {
+      // First Pokemon loses the fight
+      setHPPokemonState(0);
+      setWinner(randomPokemon.name);
+    } else {
+      // Continue with the attack on the second Pokemon
+      const newSecondPokemonState = secondPokemonState - 25;
+      setSecondPokemonState(newSecondPokemonState);
+
+      if (newSecondPokemonState <= 0) {
+        // Second Pokemon loses the fight
+        setSecondPokemonState(0);
+        setWinner(chosenPokemon.name);
+      }
+    }
+  }
+
+  ////////////
   if (isLoading || !randomPokemon) {
     // Ladezustand anzeigen oder null zurückgeben, solange die Daten geladen werden
     return <div>Loading...</div>;
@@ -50,6 +114,8 @@ export default function Arena() {
   const imageUrlChosen =
     chosenPokemon.sprites?.other?.dream_world?.front_default ||
     "https://via.placeholder.com/150";
+
+  //
 
   return (
     <div
@@ -74,15 +140,13 @@ export default function Arena() {
           <div className="mt-8 pt-8">
             <p className="text-lg font-bold text-black">Base Stats</p>
             <p className="text-base m-2 font-bold text-black">
-              HP: {chosenPokemon.stats[0].base_stat}
+              HP: {hpPokemonState}
             </p>
             <div className="relative pt-1 ">
               <div className="overflow-hidden h-4 mb-4 text-xs flex rounded-full bg-gray-50">
                 <div
                   style={{
-                    width: `${calculatePercentage(
-                      chosenPokemon.stats[0].base_stat
-                    )}%`,
+                    width: `${calculatePercentage(hpPokemonState)}%`,
                   }}
                   className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"
                 ></div>
@@ -138,9 +202,18 @@ export default function Arena() {
       </div>
       <div className="flex flex-col self-center">
         <h3 className="font-extrabold text-xxl"> VS </h3>
-        <button className=" m-16 font-bold rounded-md bg-gradient-to-r from-red-500 to-red-400 p-3 self-center border-b-4 border-red-600 hover:border-red-700 cursor-pointer transition ease-out hover:scale-125">
-          FIGHT
-        </button>
+        {winner ? (
+          <p className="font-extrabold m-10 text-red-500 text-7xl rounded bg-slate-700/50">
+            {winner} wins!
+          </p>
+        ) : (
+          <button
+            onClick={handleAttack}
+            className=" m-16 font-bold rounded-md bg-gradient-to-r from-red-500 to-red-400 p-3 self-center border-b-4 border-red-600 hover:border-red-700 cursor-pointer transition ease-out hover:scale-125"
+          >
+            FIGHT
+          </button>
+        )}
         <Link to="../highscore/">
           <button className="m-16 font-bold text-lg rounded-md bg-gradient-to-r from-red-500 to-red-400 p-3 border-b-4 border-red-600 hover:border-red-700 cursor-pointer transition ease-out hover:scale-125">
             HIGHSCORE
@@ -175,15 +248,13 @@ export default function Arena() {
               </div>
             </div>
             <p className="text-base m-1 font-bold text-black">
-              Attack: {randomPokemon.stats[1].base_stat}
+              Attack: {secondPokemonState}
             </p>
             <div className="relative pt-1">
               <div className="overflow-hidden h-4 mb-4 text-xs flex rounded-full bg-gray-50">
                 <div
                   style={{
-                    width: `${calculatePercentage(
-                      randomPokemon.stats[1].base_stat
-                    )}%`,
+                    width: `${calculatePercentage(secondPokemonState)}%`,
                   }}
                   className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-yellow-400"
                 ></div>
